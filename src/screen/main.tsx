4eb20e0e743b5b1d69ba875c6414e646bbbf15b3
adeyahya <adeyahyaprasetyo@gmail.com>
2021-04-24 10:20:53 +0700

    Search Input

--- src/screen/main.tsx
+++ src/screen/main.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import {Div} from 'react-native-magnus';
+import {Div, Input} from 'react-native-magnus';
 import {FlatList, ListRenderItem} from 'react-native';
 import {BatikListItemDTO, useBatiksQuery} from '../services/batik';
 import BatikListItem from '../components/BatikListItem';
 
-const itemSeparator = () => <Div h={0} />;
+const itemSeparator = () => (
+  <Div w="100%" px={25} alignItems="center">
+    <Div h={1} w="100%" bg="gray300" />
+  </Div>
+);
 
 const MainScreen: React.FC = () => {
   const {data} = useBatiksQuery();
+  const [isFocusSearch, setIsFocusSearch] = React.useState(false);
 
   const itemRenderer: ListRenderItem<BatikListItemDTO> = ({item}) => {
     return <BatikListItem item={item} />;
   };
 
   return (
-    <Div>
-      <FlatList
-        renderItem={itemRenderer}
-        ItemSeparatorComponent={itemSeparator}
-        keyExtractor={item => '' + item.id}
-        data={data?.data.hasil ?? []}
-      />
+    <Div flex={1} position="relative">
+      <Div
+        w="100%"
+        zIndex={1}
+        shadow={isFocusSearch ? 'md' : undefined}
+        position="absolute"
+        px={25}
+        py={10}>
+        <Input
+          onFocus={() => setIsFocusSearch(true)}
+          onBlur={() => setIsFocusSearch(false)}
+          rounded="circle"
+          placeholder="Temukan Batik"
+        />
+      </Div>
+      <Div flex={1}>
+        <FlatList
+          ListHeaderComponent={() => <Div h={70} />}
+          renderItem={itemRenderer}
+          ItemSeparatorComponent={itemSeparator}
+          keyExtractor={item => '' + item.id}
+          data={data?.data.hasil ?? []}
+        />
+      </Div>
     </Div>
   );
 };
